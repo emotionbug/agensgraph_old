bitnine-oss/agensgraph

feat: MATCH ONLY labelname
Using the ONLY keyword will limit the MATCH query to return labels without children.

* MATCH(A:v1 ONLY) RETURN A;

* MATCH()-[r:e1 ONLY]->() RETURN r;

src/backend/nodes/copyfuncs.c
@@ -4609,6 +4609,7 @@ _copyCypherNode(const CypherNode *from)
 	COPY_NODE_FIELD(variable);
 	COPY_NODE_FIELD(label);
 	COPY_NODE_FIELD(prop_map);
+	COPY_SCALAR_FIELD(inhOpt);
 
 	return newnode;
 }
@@ -4623,6 +4624,7 @@ _copyCypherRel(const CypherRel *from)
 	COPY_NODE_FIELD(types);
 	COPY_NODE_FIELD(varlen);
 	COPY_NODE_FIELD(prop_map);
+	COPY_SCALAR_FIELD(inhOpt);
 
 	return newnode;
 }

src/backend/nodes/equalfuncs.c
@@ -2954,6 +2954,7 @@ _equalCypherNode(const CypherNode *a, const CypherNode *b)
 	COMPARE_NODE_FIELD(variable);
 	COMPARE_NODE_FIELD(label);
 	COMPARE_NODE_FIELD(prop_map);
+	COMPARE_SCALAR_FIELD(inhOpt);
 
 	return true;
 }
@@ -2966,6 +2967,7 @@ _equalCypherRel(const CypherRel *a, const CypherRel *b)
 	COMPARE_NODE_FIELD(types);
 	COMPARE_NODE_FIELD(varlen);
 	COMPARE_NODE_FIELD(prop_map);
+	COMPARE_SCALAR_FIELD(inhOpt);
 
 	return true;
 }

src/backend/parser/gram.y
@@ -16088,6 +16088,26 @@ cypher_node:
 					n->variable = $2;
 					n->label = $3;
 					n->prop_map = $4;
+					n->inhOpt = true;
+					$$ = (Node *) n;
+				}
+			| '(' cypher_var_opt cypher_label_opt ONLY cypher_prop_map_opt ')'
+				{
+					CypherNode *n;
+
+					if($3 == NULL)
+					{
+						ereport(ERROR,
+							(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+							 errmsg("ONLY must have one label preceding it"),
+							 parser_errposition(@3)));
+					}
+
+					n = makeNode(CypherNode);
+					n->variable = $2;
+					n->label = $3;
+					n->prop_map = $5;
+					n->inhOpt = false;
 					$$ = (Node *) n;
 				}
 		;
@@ -16110,6 +16130,35 @@ cypher_rel:
 					n->types = $4;
 					n->varlen = $5;
 					n->prop_map = $6;
+					n->inhOpt = true;
+					$$ = (Node *) n;
+				}
+			| cypher_rel_left '[' cypher_var_opt
+			cypher_types_opt ONLY cypher_varlen_opt cypher_prop_map_opt
+			']' cypher_rel_right
+				{
+					CypherRel  *n;
+
+					if($4 == NULL)
+					{
+						ereport(ERROR,
+							(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+							 errmsg("ONLY must have one label preceding it"),
+							 parser_errposition(@3)));
+					}
+
+					n = makeNode(CypherRel);
+					if ($1)
+						n->direction |= CYPHER_REL_DIR_LEFT;
+					if ($9)
+						n->direction |= CYPHER_REL_DIR_RIGHT;
+					if ($1 && $9)
+						n->direction = CYPHER_REL_DIR_NONE;
+					n->variable = $3;
+					n->types = $4;
+					n->varlen = $6;
+					n->prop_map = $7;
+					n->inhOpt = false;
 					$$ = (Node *) n;
 				}
 		;

emotionbug/agensgraph_old

Using the ONLY keyword will limit the MATCH query to return labels
without children.
* MATCH (n:v ONLY) RETURN n;
* MATCH ()-[r:e ONLY]->() RETURN r;
Refactoring and code cleanup by Junseok Yang.
Closes #279
(cherry picked from commit 9b9c31465c6091babca8ae065f6d337619ecb938)

src/backend/nodes/copyfuncs.c
@@ -4608,6 +4608,7 @@ _copyCypherNode(const CypherNode *from)
 
 	COPY_NODE_FIELD(variable);
 	COPY_NODE_FIELD(label);
+	COPY_SCALAR_FIELD(only);
 	COPY_NODE_FIELD(prop_map);
 
 	return newnode;
@@ -4621,6 +4622,7 @@ _copyCypherRel(const CypherRel *from)
 	COPY_SCALAR_FIELD(direction);
 	COPY_NODE_FIELD(variable);
 	COPY_NODE_FIELD(types);
+	COPY_SCALAR_FIELD(only);
 	COPY_NODE_FIELD(varlen);
 	COPY_NODE_FIELD(prop_map);
 

src/backend/nodes/equalfuncs.c
@@ -2953,6 +2953,7 @@ _equalCypherNode(const CypherNode *a, const CypherNode *b)
 {
 	COMPARE_NODE_FIELD(variable);
 	COMPARE_NODE_FIELD(label);
+	COMPARE_SCALAR_FIELD(only);
 	COMPARE_NODE_FIELD(prop_map);
 
 	return true;
@@ -2964,6 +2965,7 @@ _equalCypherRel(const CypherRel *a, const CypherRel *b)
 	COMPARE_SCALAR_FIELD(direction);
 	COMPARE_NODE_FIELD(variable);
 	COMPARE_NODE_FIELD(types);
+	COMPARE_SCALAR_FIELD(only);
 	COMPARE_NODE_FIELD(varlen);
 	COMPARE_NODE_FIELD(prop_map);
 

src/backend/parser/gram.y
@@ -16076,7 +16076,25 @@ cypher_node:
 					n = makeNode(CypherNode);
 					n->variable = $2;
 					n->label = $3;
+					n->only = false;
 					n->prop_map = $4;
+					$$ = (Node *) n;
+				}
+			| '(' cypher_var_opt cypher_label_opt ONLY cypher_prop_map_opt ')'
+				{
+					CypherNode *n;
+
+					if ($3 == NULL)
+						ereport(ERROR,
+							(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+							 errmsg("ONLY must have one label preceding it"),
+							 parser_errposition(@3)));
+
+					n = makeNode(CypherNode);
+					n->variable = $2;
+					n->label = $3;
+					n->only = true;
+					n->prop_map = $5;
 					$$ = (Node *) n;
 				}
 		;
@@ -16097,8 +16115,35 @@ cypher_rel:
 						n->direction = CYPHER_REL_DIR_NONE;
 					n->variable = $3;
 					n->types = $4;
+					n->only = false;
 					n->varlen = $5;
 					n->prop_map = $6;
+					$$ = (Node *) n;
+				}
+			| cypher_rel_left '[' cypher_var_opt
+			cypher_types_opt ONLY cypher_varlen_opt cypher_prop_map_opt
+			']' cypher_rel_right
+				{
+					CypherRel  *n;
+
+					if ($4 == NULL)
+						ereport(ERROR,
+							(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+							 errmsg("ONLY must have one label preceding it"),
+							 parser_errposition(@3)));
+
+					n = makeNode(CypherRel);
+					if ($1)
+						n->direction |= CYPHER_REL_DIR_LEFT;
+					if ($9)
+						n->direction |= CYPHER_REL_DIR_RIGHT;
+					if ($1 && $9)
+						n->direction = CYPHER_REL_DIR_NONE;
+					n->variable = $3;
+					n->types = $4;
+					n->only = true;
+					n->varlen = $6;
+					n->prop_map = $7;
 					$$ = (Node *) n;
 				}
 		;