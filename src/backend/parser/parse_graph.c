bitnine-oss/agensgraph

feat: Implement the dijkstra function
* Treat it like a clause in SELECT

* Syntax
```
DIJKSTRA(pattern, weight)
DIJKSTRA(pattern, weight, qual)
DIJKSTRA(pattern, weight, LIMIT max)
DIJKSTRA(pattern, weight, qual, LIMIT max)
```

* Transform
```
path = DIJKSTRA ((source)-[:edge]->(target), weight, qual, LIMIT max)
     |
     v
SELECT (...), (...)
FROM (
  SELECT
    dijkstra_vids() as vids,
    dijkstra_eids() as eids,
    weight
  FROM  graph_path.edge
  WHERE start = id(source)
    AND qual
  DIJKSTRA (id(source), id(target), LIMIT max, "end", id)
)
```
** exclude subqueries that create a graphpath value using vids and eids.
   (See shortestpath())
** dijkstra_vids() and dijkstra_eids() are placeholders.
   These are not executed.
   The value of function is forced from the DijkstraState node.

* Optimize
** Add the upper_rel of the DIJKSTRA clause above the rel of the SubPlan.
** Here, SubPlan represents an adjacency matrix.  It is generated from
   the following query.  To preserve the appearance of the query,
   the subquery in the FROM clause with the DIJKSTRA clause is not `pull up`.
```
SELECT weight, "end", id
FROM graph_path.edge
WHERE start = id(start) // param
  AND qual
```

* DijkstraState
** Implement the classic dijkstra algorithm
   (https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
** use SubPlan instead of the adjacency matrix
** use Hash table instead of distance and path arrays
** use the pairing heap for the priority queue
** SubPlan is parameterized to the `id(source)` through
   `WHERE start = id(source)`
** For each iteration, force the `id(source)` to be reset
   and get adjacency vertices

src/backend/parser/parse_graph.c
@@ -121,9 +121,9 @@ static RangeTblEntry *transformMatchOptional(ParseState *pstate,
 											 CypherClause *clause);
 /* MATCH - preprocessing */
 static bool hasPropConstr(List *pattern);
-static List *getShortestPaths(List *pattern);
-static void appendShortestPathsResult(ParseState *pstate, List *splist,
-									  List **targetList);
+static List *getFindPaths(List *pattern);
+static void appendFindPathResult(ParseState *pstate, List *find_paths,
+								 List **targetList);
 static void collectNodeInfo(ParseState *pstate, List *pattern);
 static void addNodeInfo(ParseState *pstate, CypherNode *cnode);
 static NodeInfo *getNodeInfo(ParseState *pstate, char *varname);
@@ -324,10 +324,15 @@ transformCypherSubPattern(ParseState *pstate, CypherSubPattern *subpat)
 	Query *qry;
 	RangeTblEntry *rte;
 
-	if (subpat->kind == CSP_SHORTESTPATH)
+	if (subpat->kind == CSP_FINDPATH)
 	{
+		CypherPath *cp;
 		Assert(list_length(subpat->pattern) == 1);
-		return transformShortestPath(pstate, linitial(subpat->pattern));
+		cp = linitial(subpat->pattern);
+		if (cp->kind == CPATH_DIJKSTRA)
+			return transformDijkstra(pstate, cp);
+		else
+			return transformShortestPath(pstate, cp);
 	}
 
 	match = makeNode(CypherMatchClause);
@@ -565,16 +570,16 @@ transformCypherMatchClause(ParseState *pstate, CypherClause *clause)
 		}
 		else
 		{
-			List *splist = NIL;
+			List *find_paths = NIL;
 
-			splist = getShortestPaths(detail->pattern);
-			if (!pstate->p_is_sp_processed && splist != NULL)
+			find_paths = getFindPaths(detail->pattern);
+			if (!pstate->p_is_sp_processed && find_paths != NULL)
 			{
 				pstate->p_is_sp_processed = true;
 				rte = transformClause(pstate, (Node *) clause);
 
 				qry->targetList = makeTargetListFromRTE(pstate, rte);
-				appendShortestPathsResult(pstate, splist, &qry->targetList);
+				appendFindPathResult(pstate, find_paths, &qry->targetList);
 			}
 			else
 			{
@@ -966,9 +971,9 @@ hasPropConstr(List *pattern)
 }
 
 static List *
-getShortestPaths(List *pattern)
+getFindPaths(List *pattern)
 {
-	List	   *splist = NIL;
+	List	   *find_paths = NIL;
 	ListCell   *lp;
 
 	foreach(lp, pattern)
@@ -976,18 +981,18 @@ getShortestPaths(List *pattern)
 		CypherPath *p = lfirst(lp);
 
 		if (p->kind != CPATH_NORMAL)
-			splist = lappend(splist, p);
+			find_paths = lappend(find_paths, p);
 	}
 
-	return splist;
+	return find_paths;
 }
 
 static void
-appendShortestPathsResult(ParseState *pstate, List *splist, List **targetList)
+appendFindPathResult(ParseState *pstate, List *find_paths, List **targetList)
 {
 	ListCell *le;
 
-	foreach(le, splist)
+	foreach(le, find_paths)
 	{
 		CypherPath *p = lfirst(le);
 		char	   *pathname = getCypherName(p->variable);
@@ -1002,7 +1007,10 @@ appendShortestPathsResult(ParseState *pstate, List *splist, List **targetList)
 					(errcode(ERRCODE_SYNTAX_ERROR),
 					 errmsg("a variable name of path must be provided")));
 
-		sp = transformShortestPathInMatch(pstate, p);
+		if (p->kind == CPATH_DIJKSTRA)
+			sp = transformDijkstraInMatch(pstate, p);
+		else
+			sp = transformShortestPathInMatch(pstate, p);
 
 		alias = makeAliasOptUnique(NULL);
 		rte = addRangeTableEntryForSubquery(pstate, sp, alias, true, true);
@@ -1012,8 +1020,17 @@ appendShortestPathsResult(ParseState *pstate, List *splist, List **targetList)
 							 (AttrNumber) pstate->p_next_resno++,
 							 pathname,
 							 false);
-
 		*targetList = lappend(*targetList, te);
+
+		if (p->weight_variable)
+		{
+			pathname = getCypherName(p->weight_variable);
+			te = makeTargetEntry((Expr *) getColumnVar(pstate, rte, pathname),
+								 (AttrNumber) pstate->p_next_resno++,
+								 pathname,
+								 false);
+			*targetList = lappend(*targetList, te);
+		}
 	}
 }
 

emotionbug/agensgraph_old

The implementation treats it like a clause in SELECT.
  ```
  DIJKSTRA(pattern, weight)
  DIJKSTRA(pattern, weight, qual)
  DIJKSTRA(pattern, weight, LIMIT max)
  DIJKSTRA(pattern, weight, qual, LIMIT max)
  path = DIJKSTRA ((source)-[:edge]->(target), weight, qual, LIMIT max)
   |
   v
  SELECT (...), (...)
  FROM (
    SELECT
      dijkstra_vids() as vids,
      dijkstra_eids() as eids,
      weight
    FROM  graph_path.edge
    WHERE start = id(source) AND qual
    DIJKSTRA (id(source), id(target), LIMIT max, "end", id)
  )
  * Exclude subqueries that create a `graphpath` value using vids and
    eids. (See `shortestpath()`)
  * `dijkstra_vids()` and `dijkstra_eids()` are placeholders. These will
    not be executed. Instead, the return value of the functions are
    forced from the DijkstraState node.
  * Add the `upper_rel` of the DIJKSTRA clause above the rel of the
    `SubPlan`.
  * Here, `SubPlan` represents an adjacency matrix. It is generated from
    the following query. To preserve the appearance of the query, the
    subquery in the FROM clause with the DIJKSTRA clause is **not**
    pulled up.
  SELECT weight, "end", id
  FROM graph_path.edge
  WHERE start = id(start) # param
        AND qual
* `DijkstraState`
  * Implement [the classic dijkstra algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
  * Use `SubPlan` instead of the adjacency matrix
  * Use Hash table instead of distance and path arrays
  * Use the pairing heap for the priority queue
  * `SubPlan` is parameterized to the `id(source)` through
    `WHERE start = id(source)`
  * For each iteration, force the `id(source)` to be reset
    and get adjacency vertices
Closes #218
(cherry picked from commit d24a915f422e2427fc878a0cc80426c2a57403e8)

src/backend/parser/parse_graph.c
@@ -122,9 +122,9 @@ static RangeTblEntry *transformMatchOptional(ParseState *pstate,
 											 CypherClause *clause);
 /* MATCH - preprocessing */
 static bool hasPropConstr(List *pattern);
-static List *getShortestPaths(List *pattern);
-static void appendShortestPathsResult(ParseState *pstate, List *splist,
-									  List **targetList);
+static List *getFindPaths(List *pattern);
+static void appendFindPathsResult(ParseState *pstate, List *fplist,
+								  List **targetList);
 static void collectNodeInfo(ParseState *pstate, List *pattern);
 static void addNodeInfo(ParseState *pstate, CypherNode *cnode);
 static NodeInfo *getNodeInfo(ParseState *pstate, char *varname);
@@ -320,10 +320,16 @@ transformCypherSubPattern(ParseState *pstate, CypherSubPattern *subpat)
 	Query *qry;
 	RangeTblEntry *rte;
 
-	if (subpat->kind == CSP_SHORTESTPATH)
+	if (subpat->kind == CSP_FINDPATH)
 	{
+		CypherPath *cp;
+
 		Assert(list_length(subpat->pattern) == 1);
-		return transformShortestPath(pstate, linitial(subpat->pattern));
+		cp = linitial(subpat->pattern);
+		if (cp->kind == CPATH_DIJKSTRA)
+			return transformDijkstra(pstate, cp);
+		else
+			return transformShortestPath(pstate, cp);
 	}
 
 	match = makeNode(CypherMatchClause);
@@ -560,23 +566,23 @@ transformCypherMatchClause(ParseState *pstate, CypherClause *clause)
 		}
 		else
 		{
-			List *splist = NIL;
+			List *fplist = NIL;
 
-			splist = getShortestPaths(detail->pattern);
-			if (!pstate->p_is_sp_processed && splist != NULL)
+			fplist = getFindPaths(detail->pattern);
+			if (!pstate->p_is_fp_processed && fplist != NULL)
 			{
-				pstate->p_is_sp_processed = true;
+				pstate->p_is_fp_processed = true;
 				rte = transformClause(pstate, (Node *) clause);
 
 				qry->targetList = makeTargetListFromRTE(pstate, rte);
-				appendShortestPathsResult(pstate, splist, &qry->targetList);
+				appendFindPathsResult(pstate, fplist, &qry->targetList);
 			}
 			else
 			{
 				List *components;
 
 				pstate->p_is_match_quals = false;
-				pstate->p_is_sp_processed = false;
+				pstate->p_is_fp_processed = false;
 
 				/*
 				 * To do this at here is safe since it just uses transformed
@@ -961,9 +967,9 @@ hasPropConstr(List *pattern)
 }
 
 static List *
-getShortestPaths(List *pattern)
+getFindPaths(List *pattern)
 {
-	List	   *splist = NIL;
+	List	   *fplist = NIL;
 	ListCell   *lp;
 
 	foreach(lp, pattern)
@@ -971,22 +977,23 @@ getShortestPaths(List *pattern)
 		CypherPath *p = lfirst(lp);
 
 		if (p->kind != CPATH_NORMAL)
-			splist = lappend(splist, p);
+			fplist = lappend(fplist, p);
 	}
 
-	return splist;
+	return fplist;
 }
 
 static void
-appendShortestPathsResult(ParseState *pstate, List *splist, List **targetList)
+appendFindPathsResult(ParseState *pstate, List *fplist, List **targetList)
 {
 	ListCell *le;
 
-	foreach(le, splist)
+	foreach(le, fplist)
 	{
 		CypherPath *p = lfirst(le);
-		char	   *pathname = getCypherName(p->variable);
-		Query	   *sp;
+		char	   *pathname;
+		char	   *weightvar;
+		Query	   *fp;
 		Alias	   *alias;
 		RangeTblEntry *rte;
 		TargetEntry *te;
@@ -997,18 +1004,30 @@ appendShortestPathsResult(ParseState *pstate, List *splist, List **targetList)
 					(errcode(ERRCODE_SYNTAX_ERROR),
 					 errmsg("a variable name of path must be provided")));
 
-		sp = transformShortestPathInMatch(pstate, p);
+		if (p->kind == CPATH_DIJKSTRA)
+			fp = transformDijkstraInMatch(pstate, p);
+		else
+			fp = transformShortestPathInMatch(pstate, p);
 
 		alias = makeAliasOptUnique(NULL);
-		rte = addRangeTableEntryForSubquery(pstate, sp, alias, true, true);
+		rte = addRangeTableEntryForSubquery(pstate, fp, alias, true, true);
 		addRTEtoJoinlist(pstate, rte, true);
 
 		te = makeTargetEntry((Expr *) getColumnVar(pstate, rte, pathname),
 							 (AttrNumber) pstate->p_next_resno++,
 							 pathname,
 							 false);
-
 		*targetList = lappend(*targetList, te);
+
+		weightvar = getCypherName(p->weight_var);
+		if (weightvar != NULL)
+		{
+			te = makeTargetEntry((Expr *) getColumnVar(pstate, rte, weightvar),
+								 (AttrNumber) pstate->p_next_resno++,
+								 weightvar,
+								 false);
+			*targetList = lappend(*targetList, te);
+		}
 	}
 }
 
@@ -4670,7 +4689,7 @@ transformClauseImpl(ParseState *pstate, Node *clause, Alias *alias)
 
 	childParseState = make_parsestate(pstate);
 	childParseState->p_is_match_quals = pstate->p_is_match_quals;
-	childParseState->p_is_sp_processed = pstate->p_is_sp_processed;
+	childParseState->p_is_fp_processed = pstate->p_is_fp_processed;
 	childParseState->p_is_optional_match = pstate->p_is_optional_match;
 
 	qry = transformStmt(childParseState, clause);