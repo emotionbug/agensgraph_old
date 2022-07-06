bitnine-oss/agensgraph

feat: Implement Disable Index

src/backend/commands/dropcmds.c
@@ -119,7 +119,7 @@ RemoveObjects(DropStmt *stmt)
 
 		if (stmt->removeType == OBJECT_ELABEL ||
 			stmt->removeType == OBJECT_VLABEL)
-			CheckDropLabel(stmt->removeType, address.objectId);
+			CheckLabelDDL(stmt->removeType, address.objectId, "DROP");
 
 		/* Check permissions. */
 		namespaceId = get_object_namespace(&address);

src/include/commands/graphcmds.h
@@ -20,7 +20,7 @@ extern ObjectAddress RenameGraph(const char *oldname, const char *newname);
 extern void CreateLabelCommand(CreateLabelStmt *labelStmt,
 							   const char *queryString, ParamListInfo params);
 extern ObjectAddress RenameLabel(RenameStmt *stmt);
-extern void CheckDropLabel(ObjectType removeType, Oid laboid);
+extern void CheckLabelDDL(ObjectType type, Oid laboid, const char *command);
 extern void CheckInheritLabel(CreateStmt *stmt);
 
 extern bool RangeVarIsLabel(RangeVar *rel);
@@ -31,5 +31,6 @@ extern void CreateConstraintCommand(CreateConstraintStmt *constraintStmt,
 extern void DropConstraintCommand(DropConstraintStmt *constraintStmt,
 								  const char *queryString,
 								  ParamListInfo params);
+extern Oid DisableIndexCommand(DisableIndexStmt *disableStmt);
 
 #endif	/* GRAPHCMDS_H */

emotionbug/agensgraph_old

Code cleanup and refactoring from Junseok Yang
Closes #126
(cherry picked from commit e641304374f951cce00b5b3351008a4c6ebd530c)

src/backend/commands/dropcmds.c
@@ -119,7 +119,7 @@ RemoveObjects(DropStmt *stmt)
 
 		if (stmt->removeType == OBJECT_ELABEL ||
 			stmt->removeType == OBJECT_VLABEL)
-			CheckDropLabel(stmt->removeType, address.objectId);
+			CheckLabelType(stmt->removeType, address.objectId, "DROP");
 
 		/* Check permissions. */
 		namespaceId = get_object_namespace(&address);

src/include/commands/graphcmds.h
@@ -20,7 +20,7 @@ extern ObjectAddress RenameGraph(const char *oldname, const char *newname);
 extern void CreateLabelCommand(CreateLabelStmt *labelStmt,
 							   const char *queryString, ParamListInfo params);
 extern ObjectAddress RenameLabel(RenameStmt *stmt);
-extern void CheckDropLabel(ObjectType removeType, Oid laboid);
+extern void CheckLabelType(ObjectType type, Oid laboid, const char *command);
 extern void CheckInheritLabel(CreateStmt *stmt);
 
 extern bool RangeVarIsLabel(RangeVar *rel);
@@ -31,5 +31,7 @@ extern void CreateConstraintCommand(CreateConstraintStmt *constraintStmt,
 extern void DropConstraintCommand(DropConstraintStmt *constraintStmt,
 								  const char *queryString,
 								  ParamListInfo params);
+
+extern Oid DisableIndexCommand(DisableIndexStmt *disableStmt);
 
 #endif	/* GRAPHCMDS_H */